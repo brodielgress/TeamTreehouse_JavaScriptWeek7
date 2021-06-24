211c796426f572e3e83cef2f4aa8c13571a6f7bd
Brodie Gress <77457950+brodielgress@users.noreply.github.com>
2021-06-24 14:26:21 -0400

    Finished Closures

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
-var count = 0;
-
-function countBirds() {
-  count += 1;
-  return count + ' birds';
-}
-
-function countBirds() {
+function makeCounter(noun) {
+  var count = 0;
+  return function () {
     count += 1;
-    return count + ' dogs';
+    return count + ' ' + noun;
   }
-  
+}
---
+++ closure.js
@@ -0,0 +1,13 @@
+var birds = 3;
+
+function dogHouse() {
+	var dogs = 8;
+    function showDogs() {
+
+    }
+    return showDogs;
+}
+
+var getDogs = dogHouse();
+
+getDogs(); // 8
